feecf47a563a8f4293a4a075aee6995a9d709192
Marcelo <ma.darras@duocuc.cl>
2026-07-13 20:07:39 -0400

    Se define el host para la api

--- backend/general/settings.py
+++ backend/general/settings.py
@@ -38,7 +38,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'project-bey-production.up.railway.app'
+]
 
 
 # Application definition
